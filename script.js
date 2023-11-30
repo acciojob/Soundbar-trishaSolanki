fd12efb61eb4f6e12a92e465856e611e544e7048
trishaSolanki <141935315+trishaSolanki@users.noreply.github.com>
2023-11-30 16:09:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
 //your JS code here. If required.
+document.addEventListener('DOMContentLoaded', function(){
+	const buttons = document.querySelectorAll('.btn');
+	const stopButton = document.querySelector('.stop');
+	let audio;
+
+	//funtion to play sound
+	function playSound(sound){
+		audio = new Audio(`sounds/${sound}`);
+		audio.play();
+		
+	}
+
+	//function to stop sound
+	function stopSound(){
+		if(audio){
+			audio.pause();
+			audio.currentTime = 0;
+		}
+	}
+
+	//eventListener for plays button
+	buttons.forEach(button =>{
+		button.addEventListener('click', function() {
+			const sound = this.getAttribute('data-sound');
+			playSound(sound);
+		});
+	});
+	stopButton.addEventListener('click', stopSound);
+});
